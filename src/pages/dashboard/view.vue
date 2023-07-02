9bab4ba3bf85a2e9dbe68f2ad215cf0c8b18136f
jakeprice <jp@jakelprice.com>
2023-07-02 09:33:37 +0100

    Init

---
+++ src/pages/dashboard/view.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="prose">
+        <h1>Hey World</h1>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    console.log('Dashboard mounted')
+})
+</script>
